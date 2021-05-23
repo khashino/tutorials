6b1a9d5fa1e5071b643480e801c24eff675de88a
khashayar norouzi <40909721+khashino@users.noreply.github.com>
2021-05-23 14:57:29 +0430

    Update main.py

--- Programing/Python/daan/main.py
+++ Programing/Python/daan/main.py
@@ -1,38 +1,69 @@
 from selenium import webdriver
-
+from selenium.webdriver.common.by import By
 from selenium.webdriver.common.keys import Keys
+import time
 
 class AzadDaanAutomation():
 
-    def __init__(self,url,username,password):
+    def __init__(self, url, username, password):
         self.url = url
         self.username = username
         self.password = password
 
+
     def setUp(self):
-        self.driver = webdriver.Chrome()
+        options = webdriver.ChromeOptions()
+
+        options.add_experimental_option("prefs", { \
+            "profile.default_content_setting_values.media_stream_mic": 1,  # 1:allow, 2:block
+        })
+
+        self.driver = webdriver.Chrome(options=options)
 
     def logIn(self):
         driver = self.driver
-        driver.get(self.url)
+        driver.get("http://azad.daan.ir/")
 
-        warning = driver.find_element_by_class_name("btn-warning")
-        warning.click()
+        beflogin = driver.find_element_by_class_name("btn-primary")
+        beflogin.click()
 
-        loginElem = driver.find_element_by_name("identification_number")
-        loginElem.send_keys(self.username)
+        userElem = driver.find_element_by_id("identificationNumber")
+        userElem.send_keys(self.username)
 
-        passElem = driver.find_element_by_name("password")
+        passElem = driver.find_element_by_id("password")
         passElem.send_keys(self.password)
+        passElem.send_keys(Keys.ENTER)
 
-        submitBtn = driver.find_element_by_class_name("btn-primary")
-        submitBtn.click()
+        time.sleep(1)
+
+        btnClose = driver.find_element_by_class_name("close")
+        btnClose.click()
+
+    def findMyClass(self):
+        driver = self.driver
+        driver.get("http://azad.daan.ir/session-list#session-list")
+
+        onlineCls = driver.find_element_by_link_text("ورود دانشجو")
+        onlineCls.click()
+
+        time.sleep(5)
+
+        inputTxt = driver.find_element_by_id("message-input")
+        inputTxt.send_keys("سلام استاد")
+        inputTxt.send_keys(Keys.ENTER)
+
+
+
+
+
+
+
 
 
 
 
 if __name__ == "__main__":
-    myapp = AzadDaanAutomation("http://azad.daan.ir/","14195112137321111","")
+    myapp = AzadDaanAutomation("http://azad.daan.ir/", "14195112137321111", "test")
     myapp.setUp()
     myapp.logIn()
-
+    myapp.findMyClass()
